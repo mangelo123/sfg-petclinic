0ed44b94bb79430e42cf6d2f3476b7ba65ee1a69
Michael Angelo <mikeangelo123@gmail.com>
2020-02-20 22:08:04 -0500

    More pet clinic data layer.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Vet extends Person {
 
+	private Set<Specialty> specialities = new HashSet<>();
+
+	public Set<Specialty> getSpecialities() {
+		return specialities;
+	}
+
+	public void setSpecialities(Set<Specialty> specialities) {
+		this.specialities = specialities;
+	}
+	
+	
 }
