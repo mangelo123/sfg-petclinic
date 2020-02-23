4384eb8208d07f120fc49e9081f7308d91a38963
Michael Angelo <mikeangelo123@gmail.com>
2020-02-22 19:25:54 -0500

    Completed pet-clinic-data entity mapping

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -2,9 +2,18 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.io.Serializable;
 
-public class BaseEntity implements Serializable {
-	private Long id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	public Long getId() {
 		return id;
 	}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -3,8 +3,23 @@ package guru.springframework.sfgpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vet")
 public class Vet extends Person {
 
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(
+		name = "vet_speciality",
+		joinColumns = @JoinColumn(name = "vet_id"),
+		inverseJoinColumns = @JoinColumn(name = "specialty_id")
+	)	
 	private Set<Specialty> specialities = new HashSet<>();
 
 	public Set<Specialty> getSpecialities() {
